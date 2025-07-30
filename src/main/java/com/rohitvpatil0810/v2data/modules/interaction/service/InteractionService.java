53501975ae306a4d0346790aac40c4cd515361d7
Rohit Patil <rohitvpatil0810@gmail.com>
2025-07-30 23:30:24 +0530

    chore: minor fix

--- src/main/java/com/rohitvpatil0810/v2data/modules/interaction/service/InteractionService.java
+++ src/main/java/com/rohitvpatil0810/v2data/modules/interaction/service/InteractionService.java
@@ -19,8 +19,7 @@ public class InteractionService {
     V2DataTranscriber v2DataTranscriber;
 
     public InteractionResponse generateNotes(MultipartFile file) throws IOException {
-//        String fileURL = fileUploadService.uploadFile(file);
-        String fileURL = "https://ab405a588dda8bc8bf4b2db984150d2b.r2.cloudflarestorage.com/v2data/c534e3f1-f7a3-4383-b03f-235e3a4e567f-How%20to%20speak%20so%20that%20people%20want%20to%20listen%20%20Julian%20Treasure.mp3?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20250730T171520Z&X-Amz-SignedHeaders=host&X-Amz-Credential=97fb37091e5eadd4b3edab4feaec7761%2F20250730%2Fauto%2Fs3%2Faws4_request&X-Amz-Expires=3600&X-Amz-Signature=d5cabe8e6aa0337c15a9048d9f28e3b104bf05770a46039d1de9f26707d33d63";
+        String fileURL = fileUploadService.uploadFile(file);
 
         V2DataTranscriberResponseBody v2DataTranscriberResponseBody = v2DataTranscriber.transcribeAndGenerateNotes(fileURL);
         return InteractionResponse.builder()
