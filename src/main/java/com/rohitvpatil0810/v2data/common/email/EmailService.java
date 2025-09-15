18f0b0180b6e26dec61fb7bebad03c6a20d94b2b
Rohit Patil <rohitvpatil0810@gmail.com>
2025-09-16 00:13:40 +0530

    feat: integrate email service with JavaMailSender to send email verification email

---
+++ src/main/java/com/rohitvpatil0810/v2data/common/email/EmailService.java
@@ -0,0 +1,35 @@
+package com.rohitvpatil0810.v2data.common.email;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    @Async
+    public void sendEmail(String to, String subject, String body) {
+        try {
+            MimeMessage message = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(body, true);
+            mailSender.send(message);
+            log.debug("Email Sent Successfully with subject {} to {}", subject, to);
+        } catch (MessagingException e) {
+            log.debug("Failed to send email: ", e);
+            throw new RuntimeException("Failed to send email", e);
+        }
+    }
+}
--- src/main/java/com/rohitvpatil0810/v2data/modules/auth/service/EmailVerificationService.java
+++ src/main/java/com/rohitvpatil0810/v2data/modules/auth/service/EmailVerificationService.java
@@ -1,28 +1,37 @@
 package com.rohitvpatil0810.v2data.modules.auth.service;
 
+import com.rohitvpatil0810.v2data.common.email.EmailService;
 import com.rohitvpatil0810.v2data.common.security.TokenGenerator;
 import com.rohitvpatil0810.v2data.common.security.TokenHasher;
 import com.rohitvpatil0810.v2data.modules.auth.entity.VerificationToken;
 import com.rohitvpatil0810.v2data.modules.auth.repository.VerificationTokenRepository;
 import com.rohitvpatil0810.v2data.modules.users.entity.User;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor
 public class EmailVerificationService {
 
     final private VerificationTokenRepository verificationTokenRepository;
+    final private EmailService emailService;
 
-    public EmailVerificationService(VerificationTokenRepository verificationTokenRepository) {
-        this.verificationTokenRepository = verificationTokenRepository;
-    }
+    @Value("${app.base-url}")
+    private String baseUrl;
 
     public void sendVerificationEmail(User user) {
         String token = createTokenFromUser(user);
         log.debug("Token = {}", token);
+        String verificationLink = baseUrl + "/auth/verify?token=" + token;
+        String subject = "Verify your email";
+        String body = "<p>Click the link to verify your email:</p>" +
+                "<a href=\"" + verificationLink + "\">Verify Email</a>";
+        emailService.sendEmail(user.getEmail(), subject, body);
     }
 
     private String createTokenFromUser(User user) {
